4a35d9ea1e5cb039b5149806ee8ce50a89cdf616
James-Mccr <James_Ian_Mccracken@protonmail.com>
2022-10-21 22:54:45 +0100

    + implemented rest of keywords

--- Keywords.py
+++ Keywords.py
@@ -37,6 +37,7 @@
 
 import math as alias
 from datetime import time
+import random
 
 def inline(msg):
     print(msg, end = "")
@@ -181,6 +182,108 @@ def _if():
     if True:
         return True
 
+# include module
+def _import():
+    r = random.randint(1, 2)
+    return r > 0 and r < 3
+
+# check set contains a value
+def _in():
+    array = [ 0, 1, 2, 3, 4, 5 ]
+    return 3 in array
+
+# check object equality
+def _is():
+    x = 5
+    y = x   # is compares the address of the objects not the contents
+    # python memory manager may reuse objects such as small integers through caching
+    # as a result two variables which are not linked may reference the same address
+    return x is y
+
+# make anonymous functions
+def _lambda():
+    add = lambda a, b : a + b   # lambda is best used for local functions that are small/simple
+    x = add(add(1,2), 5)
+    return x == 8
+
+# special type for null/void values
+def _none():
+    x = None    # none is like null except it is treated as a unique type (NoneType)
+    y = 5
+    return x == None and x is not y
+
+# exposes a nested variable to outer function
+def _nonlocal():
+    x = 0
+    def local():
+        nonlocal x  # restricted form of global
+        x = 5
+    local()
+    return x == 5
+
+# logical NOT operator
+def _not():
+    return not 1 == 0
+
+# logical OR operator
+def _or():
+    true1 = True or True
+    true2 = True or False
+    true3 = False or True
+    false = False or False
+    return true1 and true2 and true3 and not false
+
+# make stub methods
+def _pass():
+    def local():
+        pass    # can also be used in classes and ifs
+    local()
+    return True
+
+# throw an exception
+def _raise():
+    try:
+        raise Exception
+    except:
+        return True
+
+# push value(s) to stack for caller to use
+def _return():
+    return 0 == 0   # evaluates expression before jump     
+
+# operator for built-in boolean
+def _true():
+    return True
+
+# start an exception handler block
+def _try():
+    try:
+        x = 10
+    finally:
+        return True
+
+# make a loop
+def _while():
+    i = 0
+    while i < 10:
+        i += 1
+    return i == 10
+
+# simplify exception handler
+def _with():
+    with open("README.md", "r") as file:    # similar to 1 line using statements
+        o = file.readline()   # in practice it shortens exception handlers (eg. file.open)
+    return o[0] == '#'
+
+# lazy iterator (evaluates expressions only when used)
+def _yield():
+    def local():
+        for i in range(100):
+            yield i
+    iterator = local()
+    while next(iterator) != 50: # next requests iterator to continue execution
+        continue
+    return True
 
 inline("and...")
 AssertPass(_and())
@@ -232,3 +335,51 @@ AssertPass(_global())
 
 inline("if...")
 AssertPass(_if())
+
+inline("import...")
+AssertPass(_import())
+
+inline("in...")
+AssertPass(_in())
+
+inline("is...")
+AssertPass(_is())
+
+inline("lambda...")
+AssertPass(_lambda())
+
+inline("none...")
+AssertPass(_none())
+
+inline("nonlocal...")
+AssertPass(_nonlocal())
+
+inline("not...")
+AssertPass(_not())
+
+inline("or...")
+AssertPass(_or())
+
+inline("pass...")
+AssertPass(_pass())
+
+inline("raise...")
+AssertPass(_raise())
+
+inline("return...")
+AssertPass(_return())
+
+inline("true...")
+AssertPass(_true())
+
+inline("try...")
+AssertPass(_try())
+
+inline("while...")
+AssertPass(_while())
+
+inline("with...")
+AssertPass(_with())
+
+inline("yield...")
+AssertPass(_yield())
